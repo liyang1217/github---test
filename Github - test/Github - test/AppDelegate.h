e923292808b49bb5ab0d4123e5fdf69a761f014b
liyang1217 <liyang911217@163.com>
2017-01-12 10:12:44 +0800

    初始化项目

---
+++ Github - test/Github - test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github - test
+//
+//  Created by BARCELONA on 2017/1/12.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github - test/Github - test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github - test
+//
+//  Created by BARCELONA on 2017/1/12.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
